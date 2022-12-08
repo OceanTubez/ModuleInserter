c0b6e98404c9ef97e5c6a8cf85fa67eef609f768
Gurarman Dhanoa <84045281+OceanTubez@users.noreply.github.com>
2022-12-08 09:22:41 -0800

    made the base script bruv
    YES BALLS

--- ModuleUIConverter.lua
+++ ModuleUIConverter.lua
@@ -1,1 +1,29 @@
-local waitingfortest - false
+local container = script.Parent
+local Assets = container.Parent.Parent:WaitForChild("Assets")
+local Servicer = game:GetService("Selection")
+
+local children = container:GetChildren()
+
+for i, button in ipairs(children) do
+  
+  local name = button.Name
+  
+  button.MouseButton1Click:Connect(function()
+  
+    local asset = Assets:FindFirstChild(name)
+      
+    if servicer:Get()[1] == nil then
+        asset.Parent = game:GetService("Workspace")
+        warn("Inserted "..asset.Name.." in Workspace!")
+        
+    else
+        
+        asset.Parent = servicer:Get()[1]
+        warn("Inserted "..asset.Name.." in "..asset.Parent.Name.."!")
+        
+    end
+      
+
+  end
+  
+end
